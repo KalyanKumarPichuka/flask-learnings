9115ffb8215ae57084837f39f980c1ded529db14
KalyanKumarPichuka <kalyankumarp369@gmail.com>
2020-06-26 22:36:20 +0530

    updated part-4/app.py and part-4/security.py & added part-4/user.py

--- part-4/app.py
+++ part-4/app.py
@@ -1,14 +1,26 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
-app.secret_key = 'kalyan'
+app.secret_key = "kalyan"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+            type =float,
+            required = True,
+            help = "This field cannot be empty!"
+        )
+    @jwt_required()
     def get(self, name):
         # for item in items:
         #     if item['name'] == name:
@@ -20,16 +32,44 @@ class Item(Resource):
     def post(self, name):
         # data = request.get_json(force=True) #shouldnt be used as doesnt look at conetnt-type
         # data = request.get_json(silent=True) #this gives none of content type is different
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('price',
+        #     type =float,
+        #     required = True,
+        #     help = "This field cannot be empty!"
+        # )
+        #data = Item.parser.parse_args() # parser is a class variable
         if next(filter(lambda x: x["name"] == name, items), None):
             return (
                 {"message": "An item with name '{}' already exists.".format(name)},
                 400,
             )
-        data = request.get_json()
+        #data = request.get_json()
+        data = Item.parser.parse_args() # parser is a class variable
         item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x["name"] != name, items))
+        return {'message' : 'Item deleted'}
+
+    def put(self, name):
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('price',
+        #     type =float,
+        #     required = True,
+        #     help = "This field cannot be empty!"
+        # )
+        data = parser.parse_args() # parser is a class variable
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {'name':name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
