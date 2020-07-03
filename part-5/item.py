077eb3416da1ee80ea798ddaad3bae5b9b69027c
KalyanKumarPichuka <kalyankumarp369@gmail.com>
2020-07-03 20:57:59 +0530

    updated part-5/item.py file

--- part-5/item.py
+++ part-5/item.py
@@ -56,6 +56,16 @@ class Item(Resource):
         data = Item.parser.parse_args()  # parser is a class variable
         item = {"name": name, "price": data["price"]}
         # items.append(item)
+        try:
+            Item.insert(item)
+        except:
+            return {'message': 'An error occurred while inserting the record'}, 500 # Internal server error
+        
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -65,7 +75,6 @@ class Item(Resource):
         connection.commit()
         connection.close()
 
-        return item, 201
 
     def delete(self, name):
         # global items
@@ -89,15 +98,53 @@ class Item(Resource):
         #     help = "This field cannot be empty!"
         # )
         data = parser.parse_args()  # parser is a class variable
-        item = next(filter(lambda x: x["name"] == name, items), None)
+        # item = next(filter(lambda x: x["name"] == name, items), None)
+        # if item is None:
+        #     item = {"name": name, "price": data["price"]}
+        #     items.append(item)
+        # else:
+        #     item.update(data)
+        item = Item.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {"name": name, "price": data["price"]}
-            items.append(item)
+            try:
+                Item.insert(updated_item)
+            except:
+                return {'message': 'An error occurred while inserting the item'}, 500 # Internal server error
+            
         else:
-            item.update(data)
-        return item
+            try:
+                Item.update(updated_item)
+            except:
+                return {'message': 'An error occurred while updating the item'}, 500 # Internal server error
+            
+
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'],item['name']))
+
+        connection.commit()
+        connection.close()
 
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name':row[0], 'price': row[1]})
+
+        connection.close()
+        
         return {"items": items}
